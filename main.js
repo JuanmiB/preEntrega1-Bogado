ae818a4fcaab782fe870db783a8a2fa67b0b3836
Juan Bogado <bogadojuanmiguel@gmail.com>
2023-04-01 14:48:18 -0300

    se optimiza el metodo de busqueda

--- main.js
+++ main.js
@@ -1,12 +1,39 @@
 const template = document.querySelector("#tapa-pokedex")
 const fragmento = document.createDocumentFragment()
+const screen = document.querySelector(".screen")
+const contenedor = document.querySelector("#contenedor")
+let buscarPokemons = document.querySelector("#buscarPokemons")
+console.log(buscarPokemons);
 
 
+// document.addEventListener("DOMContentLoaded", () => {
+// 	fetchApi()
+// })
 
-document.addEventListener("DOMContentLoaded", () => {
-	fetchApi()
+buscarPokemons.addEventListener("click", () => {
+	contenedor.appendChild(template.content)
+	const btnBuscar = document.querySelector("#busqueda")
+	btnBuscar.addEventListener("click", findPokemon)
 })
 
+
+
+const findPokemon = async ()=> {
+	let input = document.querySelector(".barraBuscar").value
+	if (arrayName.includes(`${input}`)) {
+		alert('¡El valor de entrada coincide con la información guardada!');
+		const resp = await fetch(`https://pokeapi.co/api/v2/pokemon/${input}`)
+		const datos = await resp.json()
+		console.log(datos);
+		console.log(datos.sprites.front_default);
+	} else {
+		alert('El valor de entrada no coincide con la información guardada. Intente nuevamente.')
+	}
+	console.log(`${input}`);
+}
+
+
+let arrayName = []
 const fetchApi = async () => {
 	try {
 		const resp = await fetch("https://pokeapi.co/api/v2/pokemon/")
@@ -16,32 +43,12 @@ const fetchApi = async () => {
 		console.log(arrayName);
 	} catch (error) {
 		console.log("error");
-	}
-}
-
-const contenedor = document.querySelector("#contenedor")
-let buscarPokemons = document.querySelector("#buscarPokemons")
-console.log(buscarPokemons);
+	}}
+	
+	fetchApi()
 
 
-buscarPokemons.addEventListener("click", () => {
-	contenedor.appendChild(template.content)
-})
 
-const btnBuscar = document.querySelector("#busqueda")
-
-let savedData = ['dato1', 'dato2', 'dato3'];
-
-
-btnBuscar.addEventListener("click", () => {
-	console.log("hola")
-	let input = document.querySelector(".barraBuscar").value
-	if (savedData.includes(`${input}`)) {
-		alert('¡El valor de entrada coincide con la información guardada!');
-	} else {
-		alert('El valor de entrada no coincide con la información guardada. Intente nuevamente.');
-	}
-})
 
 
 
